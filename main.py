092aeec89be8a99931f642baabf95b2a9e780434
fab-ricio <elysefabricio003@gmail.com>
2025-04-04 05:33:32 +0200

    checking if a number is positive or negative / 0

---
+++ main.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number "))
+
+if n > 0:
+    print(f"The number {n} is positive")
+
+elif n < 0:
+    print(f"The number {n} is negative")
+
+else:
+    print("The number is zero")
